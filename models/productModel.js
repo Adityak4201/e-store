50f4486efc44055e0a5cf546c98a900a40409802
Aditya Khandelwal <adityakhandelwal4201@gmail.com>
2021-09-11 04:39:42 +0000

    Added category i product schema

--- models/productModel.js
+++ models/productModel.js
@@ -1,42 +1,39 @@
-
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const Product = Schema({
-
-    coverImage:{
-        type: String,
-        default:""
-    },
-    username:{
-        type: String
-    },
-    user_id:{
-        type: String
-    },
-    productname: {
-        type: String,
-    },
-    productmetadescription: {
-        type: String,
-    },
-    productdescription: {
-        type: String,
-    },
-    price: {
-        type: Number,
-    },
-    sellprice: {
-        type: Number,
-    },
-    variation:{ type : Array , "default" : [] },
-    inventory: {
-        type: Number,
-    },
-    Item_Returnable: { type: Number },
-    comments:{ type : Array , "default" : [] },
-
+  coverImage: {
+    type: String,
+    default: "",
+  },
+  username: {
+    type: String,
+  },
+  user_id: {
+    type: String,
+  },
+  productname: {
+    type: String,
+  },
+  productmetadescription: {
+    type: String,
+  },
+  category: { type: String },
+  productdescription: {
+    type: String,
+  },
+  price: {
+    type: Number,
+  },
+  sellprice: {
+    type: Number,
+  },
+  variation: { type: Array, default: [] },
+  inventory: {
+    type: Number,
+  },
+  Item_Returnable: { type: Number },
+  comments: { type: Array, default: [] },
 });
 
-
-module.exports=mongoose.model("Product",Product);
+module.exports = mongoose.model("Product", Product);
