3c84eebad62e08b7acbd9b2e3d3287f8537d1b3b
Aditya Khandelwal <adityakhandelwal4201@gmail.com>
2021-10-31 05:01:51 +0000

    Add Visitor model and visitor API changed

--- routes/visitor.js
+++ routes/visitor.js
@@ -9,53 +9,22 @@ router.post("/addVisitor", auth, async (req, res) => {
       .status(404)
       .send({ msg: "Visitor can only be added after he is signed in!!" });
   }
-  const { seller_username, name, product_id } = req.body;
-  //   console.log(req.body.product_id);
-  const { username, phone, email } = req.user;
-  if (product_id !== undefined) {
-    // console.log(1);
-    await Visitor.findOneAndUpdate(
-      { seller_username },
-      {
-        $addToSet: {
-          visitor_info: {
-            username,
-            email,
-            phone,
-            name,
-            product_id,
-          },
-        },
-      },
-      { new: true, upsert: true }
-    )
-      .then((visitorAdded) => {
-        console.log(visitorAdded);
-        return res.send({ visitorAdded });
-      })
-      .catch((error) => {
-        console.log(error);
-        return res.status(403).json({ error });
-      });
-  } else {
-    // console.log(2);
-    await Visitor.findOneAndUpdate(
-      { seller_username },
-      {
-        $addToSet: {
-          visitor_info: { username, name, email, phone },
-        },
-      },
-      { new: true, upsert: true }
-    )
-      .then((visitorAdded) => {
-        console.log(visitorAdded);
-        return res.send({ visitorAdded });
-      })
-      .catch((error) => {
-        return res.status(403).json({ error });
-      });
-  }
+
+  const { email } = req.user;
+  const visitor = new Visitor({
+    ...req.body,
+    visitor_email: email,
+  });
+  await visitor
+    .save()
+    .then((visitorAdded) => {
+      console.log(visitorAdded);
+      return res.send({ visitorAdded });
+    })
+    .catch((error) => {
+      console.log(error);
+      return res.status(403).json({ error });
+    });
 });
 
 router.get("/getVisitors", auth, async (req, res) => {
