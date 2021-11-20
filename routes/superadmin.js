f0bbd1ebf65a5cf7ec835d21f6615d4a2aca351e
Aditya Khandelwal <adityakhandelwal4201@gmail.com>
2021-11-20 13:02:14 +0000

    SuperAdmin Controller Added

--- routes/superadmin.js
+++ routes/superadmin.js
@@ -1,59 +1,16 @@
 const express = require("express");
+const SuperAdminController = require("../controllers/superAdmin");
 const router = express.Router();
 const auth = require("../middleware/auth");
-const Profile = require("../models/sellerModel");
-const ShopProduct = require("../models/shoppingModel");
 
-router.get("/getSellerOrders", auth, async (req, res) => {
-  if (req.user.role != "superadmin") {
-    return res.status(404).json({ error: "No permission see orders' list" });
-  }
-
-  try {
-    const { username } = req.body;
-    const seller = await Profile.findOne({ username });
-    const ordersList = await ShopProduct.find({ sellerid: seller._id });
-    if (!ordersList || ordersList.length === 0) throw "No Orders";
-    return res.json({ ordersList });
-  } catch (error) {
-    return res.status(403).json({ error });
-  }
-});
-
-router.post("/getOrdersForSellerByLimit", auth, async (req, res) => {
-  if (req.user.role != "superadmin") {
-    return res.status(404).json({ error: "No permission see orders' list" });
-  }
-  try {
-    const { buyerid, page, limit } = req.body;
-    const startindex = (parseInt(page) - 1) * parseInt(limit);
-    const { username } = req.body;
-    const seller = await Profile.findOne({ username });
-    const ordersList = await ShopProduct.find({ sellerid: seller._id, buyerid })
-      .limit(limit)
-      .skip(startindex)
-      .exec();
-    if (!ordersList || ordersList.length === 0) throw "No Orders";
-    return res.json({ ordersList });
-  } catch (error) {
-    return res.status(403).json({ error });
-  }
-});
-
-router.get("/changeOrderPayStatus/:id", auth, (req, res) => {
-  if (req.user.role != "superadmin") {
-    return res.status(404).json({ error: "No permission see orders' list" });
-  }
-
-  ShopProduct.findOneAndUpdate(
-    { _id: req.params.id },
-    { $set: { "paymentmethod.pay_status": req.body.new_pay_status }},
-    {new : true}
-    )
-    .then((updatesPayStatus) => {
-      return res.send({ msg : "Order status Updated" , updatesPayStatus });
-    })
-    .catch((error) => {
-      return res.status(404).json({ error: "No Order Found" });
-    });
-});
+router.get("/getSellerOrders", auth, SuperAdminController.getSellerOrders);
+router.post(
+  "/getOrdersForSellerByLimit",
+  auth,
+  SuperAdminController.getBuyerOrdersForSellersByLim
+);
+router.get(
+  "/changeOrderPayStatus/:id",
+  auth,
+  SuperAdminController.changePaymentOrderStatus
+);
